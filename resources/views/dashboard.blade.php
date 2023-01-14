0730dac435c57c0251a20fb230dcc4b837c94cc5
Normita Burce <pipermita@gmail.com>
2023-01-14 13:37:52 +0800

    piper push on piper branch jan14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,8 @@
 <head>
     <title>Easy Visit Pass</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" />
-    <link rel="icon" href="{{ asset('Images/logo.png') }}" />
+    <link rel="icon" href="{{ asset('Images/revlogo3.jpg') }}" />
+    <link rel="stylesheet" href="./app.css"> 
 </head>
 <body>
 
@@ -17,69 +18,75 @@
 
     <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
     <link rel="stylesheet" href="{{asset('css/dataTables.bootstrap5.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp"
+        rel="stylesheet">
 
     <script src="{{asset('js/jquery.js')}}"></script>
     <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('js/dataTables.bootstrap5.min.js')}}"></script>
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">  
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Easy Visit Pass</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-
         <div class="navbar-nav">
             <div class="nav-item text-nowrap">
-                <a class="nav-link px-3" href="#">Welcome, {{ Auth::user()->name }}</a>
+                <div class="nav-link px-3" href="#">Welcome, {{ Auth::user()->name }}>
+                    <span class="material-symbols-outlined">person</span>
+                    <a class="nav-link {{ Request::segment(1) == 'profile' ? 'active' : '' }}" aria-current="page" href="/profile">Profile</a>
+                </div>
             </div>
-        </div>
-        
-
-
-
+        </div>      
     </header>
 
+    <aside>
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebarMenu" class="sidebarMenu col-md-3 col-lg-2 d-md-block sidebar collapse ">
                 <div class="position-sticky pt-3">
                     <ul class="nav flex-column">
                         <li class="nav-item">
+                            <span class="material-symbols-outlined"> grid_view</span>
                             <a class="nav-link {{ Request::segment(1) == 'dashboard' ? 'active' : '' }}" aria-current="page" href="/dashboard">Dashboard</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ Request::segment(1) == 'profile' ? 'active' : '' }}" aria-current="page" href="/profile">Profile</a>
-                        </li>
+                        
                         @if(Auth::user()->type != 'User')
+
                         <li class="nav-item">
+                            <span class="material-symbols-outlined">room_preferences</span>
                             <a class="nav-link {{ Request::segment(1) == 'department' ? 'active' : '' }}" aria-current="page" href="/department">Departments</a>
                         </li>
+                            
                         @endif
                         @if(Auth::user()->type == 'Admin')
                         <li class="nav-item">
+                            <span class="material-symbols-outlined">subtitles</span>
                             <a class="nav-link {{ Request::segment(1) == 'designation' ? 'active' : '' }}" aria-current="page" href="/designation">Designations</a>
                         </li>
+                            
                         @endif
                         @if(Auth::user()->type != 'User')
                         <li class="nav-item">
-                            <a class="nav-link {{ Request::segment(1) == 'sub_user' ? 'active' : '' }}" aria-current="page" href="/sub_user">Employees</a>
+                            <span class="material-symbols-outlined">badge</span>
+                            <a class="nav-link {{ Request::segment(1) == 'sub_user' ? 'active' : '' }}" aria-current="page" href="/sub_user">Employees</a>    
                         </li>
-                        @endif
-                     
-              
-
+                        
+                        @endif           
                         <li class="nav-item">
+                            <span class="material-symbols-outlined">nest_doorbell_visitor</span>
                             <a class="nav-link {{ Request::segment(1) == 'visitor' ? 'active' : '' }}" href="/visitor">Visitors</a>
-                        </li>
+                        </li>    
                         <li class="nav-item">
+                            <span class="material-symbols-outlined">how_to_reg</span>
                             <a class="nav-link {{ Request::segment(1) == 'register' ? 'active' : '' }}" href="/register">Pre-Registers</a>
                         </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}">Logout</a>
-                            </li>
-
+                        <li class="nav-item">
+                            <span class="material-symbols-outlined">logout</span>
+                            <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                        </li>
                     </ul>
 
                 </div>
@@ -92,7 +99,8 @@
                 <!--</div>!-->
             </main>
         </div>
-    </div>    
+    </div> 
+    </aside>   
 
     @endguest
 
